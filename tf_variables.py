6b0d47b170528b9945a986e23697aa6f56254fe3
ddimri <deepak_dimri@hotmail.com>
2017-01-03 15:44:25 +0530

    Update tf_variables.py

--- tf_variables.py
+++ tf_variables.py
@@ -5,7 +5,7 @@ data = data.replace(" ", "")
 data = data.replace("=", ":")
 data = data.replace("[", "").replace("]\n", "")
 data = data.replace(":\n", ":").replace(":", ": ")
-index = data.find("ipa_master_private_dns")
+index = data.find("ipa_master_1_private_dns")
 domain_name = data[index+24:]
 domain_name = domain_name[domain_name.find(".")+1:domain_name.find("\n")]
 realm_name = domain_name.upper()
